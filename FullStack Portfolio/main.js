48e91274095b6dcb3b97183d651c9a57689b6efd
kamalAbouKarroum <120580783+kamalAbouKarroum@users.noreply.github.com>
2024-05-02 18:10:39 +0300

    Add files via upload

---
+++ FullStack Portfolio/main.js
@@ -0,0 +1,8 @@
+var typed = new Typed(".text", {
+    strings:["Full Stack Developer" , "Web Developer", "Cyber Security Expert"] ,
+    typeSpeed:50,
+    backSpeed:50,
+    backDelay:500,
+    loop:true
+
+});
